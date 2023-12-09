ebac2e676feb159c451ac19051e062f80287171b
imran9i <152281834+imran9i@users.noreply.github.com>
2023-12-09 22:44:35 +0530

    Create program_to_check_wheather_the_alphabet_is_a_vowel_or_consonant.c

---
+++ program_to_check_wheather_the_alphabet_is_a_vowel_or_consonant.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    char ch;
+    printf("enter character");
+    scanf("%c",&ch);
+    if(ch=='a'){
+        printf("VOWEL");
+    }
+     else if(ch=='e'){
+        printf("VOWEL");
+    }
+    else if(ch=='i'){
+        printf("VOWEL");
+    }
+     else if(ch=='o'){
+        printf("VOWEL");
+    }
+     else if(ch=='u'){
+        printf("VOWEL");
+    }
+     else {
+        printf("CONSONANT");
+    }
+    return 0;
+}
